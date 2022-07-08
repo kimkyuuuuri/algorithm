8330b2ddbf0bd5d0ea39c8d1830879e810cb38c8
kimkyuuuuri <rnflrnfl0633@naver.com>
2022-07-08 11:18:32 +0900

    피보나치 수열

---
+++ 동적 프로그래밍/피보나치 수열.py
@@ -0,0 +1,24 @@
+#탑 다운 (재귀)
+d = [0] * 100
+
+def fibo(x):
+	if x==1 or x==2:
+    	return 1
+    
+    if d[x] != 0:
+    	return d[x]
+    d[x] = fibo(x-1) + fibo(x-2)
+    return d[x]
+    
+print(fibo(99))
+
+#보텀 업 (반복)
+d = [0] * 100
+d[1]=1
+d[2]=1
+n=99
+
+for i in range(3,n+1):
+	d[i] = d[i-1] + d[i-2]
+    
+print(d[n])
