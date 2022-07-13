9c70224cac560ba82ef4c803c557d771cc98752e
kimkyuuuuri <rnflrnfl0633@naver.com>
2022-07-13 14:59:19 +0900

    greedy

---
+++ greedy/동전0.py
@@ -0,0 +1,14 @@
+n,k=map(int,input().split())
+array=[]
+result=0
+for i in range(n):
+    array.append(int(input()))
+
+for j in range(n-1,-1,-1):
+    print(j)
+    result+=k//array[j]
+    k=k%array[j]
+
+print(result)
+    
+    
