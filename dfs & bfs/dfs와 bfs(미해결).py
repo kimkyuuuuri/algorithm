f529f43beb2cf55ff1592d7394b450b39eb4d035
kimkyuuuuri <rnflrnfl0633@naver.com>
2022-07-20 20:34:10 +0900

    dfs bfs

--- dfs & bfs/dfs와 bfs(미해결).py
+++ dfs & bfs/dfs와 bfs(미해결).py
@@ -23,7 +23,7 @@ visited = [False] * 9
 dfs(graph,1,visited)
 
 visited2=[False]*(n+1)'''
-def dfs(graph, start_node):
+'''def dfs(graph, start_node):
  
     ## 기본은 항상 두개의 리스트를 별도로 관리해주는 것
     need_visited, visited = list(), list()
@@ -48,7 +48,32 @@ def dfs(graph, start_node):
             need_visited.extend(graph[node])
             
     return visited
-
+'''
+def dfs(graph, start_node):
+    ## deque 패키지 불러오기
+    from collections import deque
+    visited = []
+    need_visited = deque()
+    
+    ##시작 노드 설정해주기
+    need_visited.append(start_node)
+    
+    ## 방문이 필요한 리스트가 아직 존재한다면
+    while need_visited:
+        ## 시작 노드를 지정하고
+        node = need_visited.pop()
+        
+ 
+        ##만약 방문한 리스트에 없다면
+        if node not in visited:
+ 
+            ## 방문 리스트에 노드를 추가
+            visited.append(node)
+            print(node)
+            ## 인접 노드들을 방문 예정 리스트에 추가
+            need_visited.extend(graph[node])
+                
+    return visited
 
 dfs(graph,1)
 
---
+++ dfs & bfs/바이러스.py
@@ -0,0 +1,41 @@
+n=int(input())
+e=int(input())
+visited=[False]*(n+1)
+graph=[]
+graph.append([])
+for i in range(e):
+    graph.append(list(map(int, input().split())))
+for i in range(n-e):
+    graph.append([])
+
+
+print(graph)
+visited2=[False]*(n+1)
+from collections import deque
+def bfs(graph,start,visited2):
+    result=0
+    queue=deque([start])
+    visited2[start]=True
+    count=0
+    while queue:
+        v=queue.popleft()
+        print(graph[v])
+        if graph[v] == []:
+           continue;
+        
+        result+=1
+        
+        for i in graph[v]:
+        
+            if not visited2[i]:
+                
+                if not visited2[graph[v][0]] and not visited2[graph[v][1]] and count!=0:
+                    
+                    return result-1
+                queue.append(i)
+                visited2[i]=True
+                count+=1
+    return result-1
+
+result=bfs(graph,1,visited2)
+print(result)
