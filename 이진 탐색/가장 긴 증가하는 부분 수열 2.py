401951e8e055eb2788ee30026a1ba64f49c60c9c
kimkyuuuuri <rnflrnfl0633@naver.com>
2022-07-29 23:32:36 +0900

    이진 탐색

---
+++ 이진 탐색/가장 긴 증가하는 부분 수열 2.py
@@ -0,0 +1,26 @@
+n=int(input())
+
+array=list(map(int,input().split()))
+
+import sys
+from bisect import bisect_left
+
+lis = []
+ans = 0
+
+for num in array:
+    if not lis:
+       
+        lis.append(num)
+        ans += 1
+        continue
+    
+    if lis[-1] < num:
+        lis.append(num)
+        ans += 1
+    else:
+        index = bisect_left(lis, num)
+        lis[index] = num
+    ㅇ
+    
+print(ans)
