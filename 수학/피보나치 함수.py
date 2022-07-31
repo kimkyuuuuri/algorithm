54140ad5b9486a077dd488d538351bf96bd21d97
kimkyuuuuri <rnflrnfl0633@naver.com>
2022-07-31 21:40:26 +0900

    동적 프로그래밍

---
+++ 수학/피보나치 함수.py
@@ -0,0 +1,22 @@
+n=int(input())
+
+def fibo(x):
+    if(x==0):
+        return 0
+    if x==1 or x==2:
+        return 1
+ 
+    if d[x] != 0:
+    	return d[x]
+    d[x] = fibo(x-1) + fibo(x-2)
+    return d[x]
+
+for i in range(n):
+    d=[0]*42
+    a=int(input())
+    if(a==0):
+        print(1,end=' ')
+    else:
+        print(fibo(a-1), end=' ')
+    print(fibo(a))
+
