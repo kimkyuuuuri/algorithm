c6804d69ef58d081aa4ace70e512e7001bec25db
kimkyuuuuri <rnflrnfl0633@naver.com>
2022-08-01 10:32:55 +0900

    수학

---
+++ 수학/터렛.py
@@ -0,0 +1,9 @@
+n=int(input())
+for i in range(n):
+    array=list(map(int,intput().split()))
+
+#임의의 위치 a,b
+#array[0], array[1]에서 a,b 까지의 거리는 13
+#array[3], array[4]에서 a,b 까지의 거리는 37...
+    
+    
