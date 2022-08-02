2a5585a9e02722c908ca3dd1554097766fffa019
kimkyuuuuri <rnflrnfl0633@naver.com>
2022-08-02 21:23:01 +0900

    수학

--- 수학/터렛.py
+++ 수학/터렛.py
@@ -1,9 +1,21 @@
+
+import math 
 n=int(input())
 for i in range(n):
-    array=list(map(int,intput().split()))
+    array=list(map(int,input().split()))
+    distance=math.sqrt((array[0]-array[3])**2+(array[1]-array[4])**2)
 
-#임의의 위치 a,b
-#array[0], array[1]에서 a,b 까지의 거리는 13
-#array[3], array[4]에서 a,b 까지의 거리는 37...
+    if (array[2]==array[5] and distance==0):
+        print(-1)
+#내접 외접
     
-    
+    elif abs(array[2]-array[5])==distance or array[2]+array[5]==distance:
+        print(1)
+
+    elif abs(array[2]-array[5])<distance<array[2]+array[5]:
+        print(2)
+    else:
+        print(0)
+
+
+
