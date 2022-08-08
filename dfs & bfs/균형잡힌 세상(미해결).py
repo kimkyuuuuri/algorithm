6e52539e934dabe3fce054737eb263e5ad692178
kimkyuuuuri <rnflrnfl0633@naver.com>
2022-08-08 23:10:24 +0900

    큐

---
+++ dfs & bfs/균형잡힌 세상(미해결).py
@@ -0,0 +1,53 @@
+from collections import deque
+import math
+
+while True:
+    array=[]
+    a=list(input())
+    dq=deque()
+  
+    
+    result='yes'
+    if a[0]=='.' and len(a)==1:
+        break
+    for i in a:
+        if i=='[' or i==']' or i=='(' or i==')':
+            dq.append(i)
+            
+    for i in range(math.floor(len(dq)/2)):
+        
+        st=dq.pop()
+        
+        if(dq[-1]=='[' and st==']'):
+            dq.pop()
+            
+            continue
+        elif(dq[-1]=='(' and st==')'):
+            dq.pop()
+        
+            continue
+        
+        elif(dq[-0]=='[' and st==']'):
+            dq.popleft()
+            
+          
+            continue
+        elif(dq[-0]=='(' and st==')'):
+            dq.popleft()
+         
+            continue
+        else:
+            
+            result='no'
+        if len(dq)==0:
+            break
+        
+            
+    
+    if(len(dq)!=0):
+        result='no'
+    print(result)
+      
+   
+
+#(([ (([( [ ] ) ( ) (( ))] )) ]). 해결 안됨
