c5a528ffb0bcb14453d37be52c79ab845e2e69a7
kimkyuuuuri <rnflrnfl0633@naver.com>
2022-08-18 13:00:05 +0900

    동적 프로그래밍

--- 동적 프로그래밍/신나는 함수 실행.py
+++ 동적 프로그래밍/신나는 함수 실행.py
@@ -1,9 +1,15 @@
 #재귀 함수 호출하는 건데, 배열을 이용해서 한번 구한 내용은 더이상 구하지 않도록 하는 것이 핵심 !
 #문제는 어떤 배열을 선언해야하지?
+import sys
+sys.setrecursionlimit(10**6)
+#재귀 함수 호출하는 건데, 배열을 이용해서 한번 구한 내용은 더이상 구하지 않도록 하는 것이 핵심 !
+#문제는 어떤 배열을 선언해야하지?
 
 def w(a,b,c):
     global array
     if a<=0 or b<=0 or c<=0:
+        
+            
         return 1
     elif a>20 or b>20 or c>20:
         w(a,b,c)
@@ -24,14 +30,25 @@ while True:
     
     if inputa==-1 and inputb==-1 and inputc==-1:
         break
+    elif inputa<=0 or inputb<=0 or inputc<=0:
+        print( "w(" +str(inputa)+", " +str(inputb)+ ", " + str(inputc) +") = "+ str(1))
+            
+    else:
    
-    w(inputa,inputb,inputc)
-    array[0][0][0]=3
+        w(inputa,inputb,inputc)
+
     
-    print( "w(" +str(inputa)+", " +str(inputb)+ ", " + str(inputc) +") = "+ str(array[inputa][inputb][inputc]))
+        print( "w(" +str(inputa)+", " +str(inputb)+ ", " + str(inputc) +") = "+ str(array[inputa][inputb][inputc]))
     
 
 #배열은 만들어둠! 음 어떻게 저장할지 고민해보기
+
+    
+
+  
+#배열은 만들어둠! 음 어떻게 저장할지 고민해보기
 #현재 문제, 다 0으로 들어가고 함수가 한번밖에 호출이 안되고 있음 (재귀 X) -> 재귀적으로 호출이 안되는 것이 가장 큰 문제!
 #이제 음수 처리해야함!!
+#음수이면 인덱스를 50 up 해서 넣을까?
+#메모리 초과 뜸!
     
