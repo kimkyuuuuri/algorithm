1c3c521ee5ad3c782b616b7352be864a3e1b1f85
kimkyuuuuri <rnflrnfl0633@naver.com>
2022-08-23 09:56:18 +0900

    수학

--- 기타/알파벳 찾기.py
+++ 기타/알파벳 찾기.py
@@ -1,6 +1,6 @@
 n=input()
 array = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-#그냥 배열에 숫자 넣어두고 find 함수 사용하 ->즐겨찾기에 있음
+
 for i in array:
     val=n.find(i)
     print(val,end=' ')
---
+++ 수학/소수구하기.py
@@ -0,0 +1,2 @@
+a,b =map(int,input().split())
+#정수로 변환할 때 map 사용!
