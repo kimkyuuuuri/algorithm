eb4281209d87870c00b4899f31a535da93eba55e
kimkyuuuuri <rnflrnfl0633@naver.com>
2022-08-24 09:55:03 +0900

    소수구하기

--- 수학/소수구하기.py
+++ 수학/소수구하기.py
@@ -1,12 +1,12 @@
 a,b =map(int,input().split())
 #해결, 2부터 i 제곱근까지 검사하면 된다
 #for문 안에 범위설정할 수 있다.
-# isPrime함수에서 2부터 i의 제곱근까지 검사하면 된다고 함..고민해보기
+# isPrime함수에서 2부터 i의 제곱근까지 검사하면 된다고 함..고민해보
 
 def isPrime(num):
     number=2
-    for i in range(num-1):
-        if num%number==0:
+    for i in range(2,int(num**0.5)+1):
+        if num%i==0:
             number+=1
             return False
 
