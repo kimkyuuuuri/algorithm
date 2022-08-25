c78c58793a2ea4bea6a8f99e22b51bbf488a84bb
kimkyuuuuri <rnflrnfl0633@naver.com>
2022-08-25 09:52:30 +0900

    수학

--- 수학/소수구하기.py
+++ 수학/소수구하기.py
@@ -4,17 +4,15 @@ a,b =map(int,input().split())
 # isPrime함수에서 2부터 i의 제곱근까지 검사하면 된다고 함..고민해보
 
 def isPrime(num):
-    number=2
     for i in range(2,int(num**0.5)+1):
         if num%i==0:
-            number+=1
             return False
 
     return True
 
 for i in range(a,b+1):
-    
-    if isPrime(a):
-        print(a)
-    a+=1
-        
+    if i==1:
+        continue
+    if isPrime(i):
+        print(i)
+   
