a9c824443fe265d565fd69adff38385e36d50c23
kimkyuuuuri <rnflrnfl0633@naver.com>
2023-07-22 15:04:50 +0900

    0722

---
+++ practice/0722/괄호변환.py
@@ -0,0 +1,50 @@
+# 균형잡힌 괄호 문자열 인덱스 return
+def balanced_index(p):
+    count=0
+    for i in range(len(p)):
+        if p[i] =='(':
+            count+=1
+        else:
+            count-=1
+        
+        if count==0:
+            return i
+
+def right_string(p):
+    count=0
+    for i in p:
+        if i=='(':
+            count+=1
+        else:
+            count-=1
+        if count<0:
+            return False    
+    return True
+    
+
+
+
+def solution(p):
+    answer = ''
+    if p=='':
+        return answer
+    index= balanced_index(p)
+    u=p[:index+1]
+    v=p[index+1:]
+    
+    if right_string(u):
+        answer=u+solution(v)
+    else:
+        answer='('
+        answer+=solution(v)
+        answer+=')'
+        u=list(u[1:-1])
+        for i in range(len(u)):
+            if u[i]=='(':
+                u[i]=')'
+            else:
+                u[i]='('
+        answer += ''.join(u)
+ 
+   
+    return answer
