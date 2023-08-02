340ad34c2f6ce06db6b1efab2b5bdd4c0e3648fe
kimkyuuuuri <rnflrnfl0633@naver.com>
2023-08-02 21:38:57 +0900

    0802

--- practice/0802/미래도시.py
+++ practice/0802/미래도시.py
@@ -1,32 +1,31 @@
-INF=int(1e9)
-# 노드수 
-n=int(input())
-# 간선수 
-m=int(input())
+INF = int(1e9)
+n, m = map(int,input().split())
 
 
-graph = [[INF]*(n+1) for _ in range(n+1)]
+graph = [[INF]* (n+1) for _ in range(n+1)]
 
-for i in range(n+1):
-    for j in range(n+1):
+for i in range(1,n+1):
+    for j in range(1,n+1):
         if i==j:
             graph[i][j]=0
 
+for i in range(m):
+    a,b = map(int, input().split())
+    graph[a][b]=1
+    graph[b][a]=1
 
-for _ in range(m):
-    a,b,c= map(int, input().split())
-    graph[a][b]=c
 
 for k in range(1,n+1):
     for i in range(1,n+1):
         for j in range(1,n+1):
-            graph[i][j]=min(graph[i][j], graph[i][k]+graph[k][j])
+            graph[i][j]=min(graph[i][j],graph[i][k]+graph[k][j])
 
-for i in range(1,n+1):
-    for j in range(1,n+1):
-        if graph[i][j]==1e9:
-            print("무한")
-        else:
-            print(graph[i][j],end=' ')
-    print()
+x,k = map(int, input().split())
+
+result=graph[1][k]+graph[k][x]
+
+if result <= INF:
+    print(result)
+else:
+    print(-1)
     
