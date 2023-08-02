4f5043ce2422f8dc5c44a9f79444bc6f2bbbf7ec
kimkyuuuuri <rnflrnfl0633@naver.com>
2023-08-02 20:48:56 +0900

    미래도시

---
+++ practice/0802/미래도시.py
@@ -0,0 +1,32 @@
+INF=int(1e9)
+# 노드수 
+n=int(input())
+# 간선수 
+m=int(input())
+
+
+graph = [[INF]*(n+1) for _ in range(n+1)]
+
+for i in range(n+1):
+    for j in range(n+1):
+        if i==j:
+            graph[i][j]=0
+
+
+for _ in range(m):
+    a,b,c= map(int, input().split())
+    graph[a][b]=c
+
+for k in range(1,n+1):
+    for i in range(1,n+1):
+        for j in range(1,n+1):
+            graph[i][j]=min(graph[i][j], graph[i][k]+graph[k][j])
+
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        if graph[i][j]==1e9:
+            print("무한")
+        else:
+            print(graph[i][j],end=' ')
+    print()
+    
